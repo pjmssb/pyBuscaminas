1b4c7a1f59276d2cd355a1808c2604d6a70439be
Pablo J. Mogollón <pjmssb@hotmail.com>
2022-06-22 20:31:19 +0200

    Primer frame de Tkinter

--- pyBuscaminas.py
+++ pyBuscaminas.py
@@ -1,6 +1,19 @@
 from tkinter import *
 
 root = Tk()
+#Setting the window
+root.configure(bg="black")
 root.geometry('1440x720')
 root.title('Juego de buscaminas')
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg="silver", 
+    width=1440,
+    height=180
+)
+top_frame.place(x=0, y=0)
+
+
 root.mainloop()
